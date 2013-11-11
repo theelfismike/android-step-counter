d4711911fd18f32848c0e83eebbc0da621c23437
Mike Kelley <theelfismike@gmail.com>
2013-11-10 20:50:01 -0600

    Keep receiving events.

--- src/com/starboardland/pedometer/CounterActivity.java
+++ src/com/starboardland/pedometer/CounterActivity.java
@@ -2,10 +2,7 @@ package com.starboardland.pedometer;
 
 import android.app.Activity;
 import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.hardware.*;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,6 +11,7 @@ public class CounterActivity extends Activity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private TextView count;
+    boolean activityRunning;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,23 +25,29 @@ public class CounterActivity extends Activity implements SensorEventListener {
     @Override
     protected void onResume() {
         super.onResume();
+        activityRunning = true;
         Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         if (countSensor != null) {
-            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
         } else {
             Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
         }
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(this);
+        activityRunning = false;
+//        sensorManager.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-           count.setText(String.valueOf(event.values[0]));
+        if (activityRunning) {
+            count.setText(String.valueOf(event.values[0]));
+        }
+
     }
 
     @Override
